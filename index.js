ca7a5e1388bab56006108770a189579c03ad1db2
peter parker <thisisjustpeter@gmail.com>
2023-04-21 16:49:55 +0900

    (feat): Implement domControllers and apiControllers for testing api server

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-import { showForm, hideForm } from "./controllers/domControllers.js";
+import { showForm, hideForm, renderPosts, editPost } from "./controllers/domControllers.js";
+import { createPost, updatePost, deletePost } from "./controllers/apiControllers.js";
+
+document.addEventListener('DOMContentLoaded', () => {
+  renderPosts();
+});
 
 const postBtn = document.getElementById('postBtn');
 const cancelBtn = document.getElementById('cancelBtn');
@@ -9,5 +14,47 @@ postBtn.addEventListener('click', () => {
 });
 
 cancelBtn.addEventListener('click', () => {
-  hideForm(form)
+  hideForm(form);
 })
+
+
+const submitBtn = document.getElementById('submitBtn');
+
+submitBtn.addEventListener('click', () => {
+  const title = document.getElementById('form-title').value;
+  const content = document.getElementById('form-content').value;
+
+  if (title && content) {
+    createPost(title, content)
+      .then(() => alert('Sucess!'))
+      .then(() => {
+        hideForm(form);
+        renderPosts();
+      });
+  }
+})
+
+const postsContainer = document.getElementById('posts-container');
+postsContainer.addEventListener('click', (event) => {
+  // Check if the clicked element is a delete button
+  if (event.target.classList.contains('deleteBtn')) {
+    const id = event.target.getAttribute('data-id');
+    console.log(id)
+    deletePost(id)
+      .then(() => renderPosts());
+  } else if (event.target.classList.contains('updateBtn')) {
+    editPost(event);
+  } else if (event.target.classList.contains('saveBtn')) {
+    const id = event.target.getAttribute('data-id');
+    const title = document.querySelector('.edit-title').value;
+    const content = document.querySelector('.edit-content').value;
+    updatePost(id, title, content)
+      .then(() => renderPosts());
+  } else if (event.target.classList.contains('undoBtn')) {
+    const undoBtn = document.querySelector('.undoBtn');
+    undoBtn.addEventListener('click', () => {
+      alert('click')
+      renderPosts();
+    });
+  }
+});
